8cc3eb77193cbc9029f8a1caa535819fc57f6d65
YuvalZym <yuvizm@gmail.com>
2017-10-09 13:11:43 +0300

    d
    update 2

--- index.js
+++ index.js
@@ -89,6 +89,7 @@ controller.hears('hello', 'direct_message', function (bot, message) {
     bot.reply(message, 'Hello!');
 });
 
+controller.hears(['hello', 'hi', 'greetings'], ['direct_mention', 'mention', 'direct_message'], function(bot,message) {
     bot.reply(message, 'Hello!');
 });
 
